2af684d27e32a6befecf2332109a0156b885974e
AhmedEzzat96 <61329281+AhmedEzzat96@users.noreply.github.com>
2020-02-29 02:08:30 +0200

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  MediaFinder
+//
+//  Created by Ahmed Ezzat on 2/28/20.
+//  Copyright © 2020 Ahmed Ezzat. All rights reserved.
+//
+
+import Foundation
+// Cells
+struct Cells {
+    static let movieCell = "MoviesCell"
+}
+
+// Storyboards
+struct Storyboards {
+    static let main = "Main"
+}
+
+// ViewControllers
+struct VCs {
+    static let signUpVC = "SignUpVC"
+    static let signInVC = "SignInVC"
+    static let profileVC = "ProfileVC"
+    static let mapVC = "MapVC"
+    static let moviesListVC = "MoviesListVC"
+    static let movieDetailsVC = "MovieDetailsVC"
+}
+
+struct UserDefaultsKeys {
+    static let isLoggedIn = "isLoggedIn"
+    static let userModel = "userModel"
+}
